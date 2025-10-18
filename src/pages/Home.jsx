ff4165b0fcb9d13425adb99b485e00fbd8f0b97d
M Juma <qasimimohammad819@gmail.com>
2025-10-18 20:34:28 +0200

    create hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { toast } from 'react-toastify';
 import OfferSection from "../component/Offer";
 import { useEffect, useState } from "react";
-import hero from "../assets/hero.jpg"
+import hero from "../assets/hero-img.jpg"
 import MyCustomer from "../component/MyCustomer";
 const Home = () => {
   const [products, setProducts] = useState([])
@@ -19,15 +19,15 @@ const Home = () => {
   }
   return (
     <div>
-      <div className="w-full bg-gray-100 text-black min-h-[80vh] flex flex-col justify-center
-    bg-[url('https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')] bg-cover bg-center]
-    ">
+        <div className="w-full text-black min-h-[80vh] flex flex-col justify-center bg-cover bg-center] "
+              style={{ backgroundImage: `url(${hero})` }}
+          >
 
-        <div></div>
-        <div>
-          <h1 className="text-4xl font-bold ">ONline Shop</h1>
-          <h1 className="text-2xl">Lorem, ipsum dolor sit amet consec repudiandae nulla placeat voluptatum voluptas? Tempora miatur vero quae explicabo.</h1>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"><Link to="/products">Shop Now</Link></button>
+        
+        <div className="text-gray-900 text-left w-[80%] mx-auto text-slate-800">
+          <h1 className="text-4xl font-bold ">Online Shop</h1>
+          <h1 className="text-2xl text-slate-300">Lorem, ipsum dolor sit amet consec repudiandae nulla placeat voluptatum voluptas? Tempora miatur vero quae explicabo.</h1>
+          <button className="bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded"><Link to="/products">Shop Now</Link></button>
 
         </div>
       </div>
@@ -37,10 +37,17 @@ const Home = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4">
           {products.map(product => (
             <div key={product.id} className="shadow-lg p-2 rounded-sm shadow-mdtransform transition-transform hover:-translate-y-1 hover:shadow-2xl">
+              <Link to={"/products/" + product.id} key={product.id}>
               <img src={product.image} alt={product.title} className="w-[80%] h-38 object-contain mb-4 " />
-              <h2 className="text-lg font-semibold">{product.title.slice(0, 20)}</h2>
+              </Link>
+              <h2 className="text-lg font-semibold text-gray-800">{product.title.slice(0, 20)}</h2>
               <p className="text-gray-600">${product.price}</p>
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4" onClick={addToCart}>Add to Cart</button>
+              <button className="bg-pink-500 hover:bg-pink-600 text-white font-bold py-2 px-4 rounded mt-4" onClick={addToCart}>Add to Cart</button>
+           <div>
+            <h1 className="animate-dicount-show bg-red-600 rounded-md text-white">
+            30% discount for you
+           </h1>
+           </div>
             </div>
 
           ))
