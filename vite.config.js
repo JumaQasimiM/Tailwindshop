29d3ddc54f31adaa714049928645453810886c37
M Juma <qasimimohammad819@gmail.com>
2025-11-02 19:12:05 +0100

    run build

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/Tailwindshop/', // für github pages Online
-  plugins: [
-    tailwindcss(),
-    react()],
-})
-
-
-
+  base: "/Tailwindshop/", // für github pages Online
+  plugins: [tailwindcss(), react()],
+});
